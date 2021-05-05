f1b95f0c359cad4b91c6d02d15c826dfaaf438a6
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-05-05 19:55:11 +0800

    Add files via upload

---
+++ 计算机系.sql
@@ -0,0 +1,8 @@
+﻿SELECT S_NAME, SCORE
+FROM Choice,Student,Course
+WHERE Choice.COURSE_NO = Course.COURSE_NO AND Student.S_NO = Choice.S_NO AND  Choice.COURSE_NO = ANY
+(
+SELECT  COURSE_NO
+FROM Course 
+WHERE COURSE_NAME='程序设计语言'
+) 
