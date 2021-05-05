119d13bac5382d8b9ed8b4912c1f2f8225bd3e9c
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-05-05 19:54:51 +0800

    Add files via upload

---
+++ 程序设计语言.sql
@@ -0,0 +1,6 @@
+SELECT Student.S_NO,S_NAME,CLASS_NAME,Course.COURSE_NO,COURSE_NAME,SCORE
+FROM Student,Class,Course,Choice
+WHERE (Course.COURSE_NO=Choice.COURSE_NO)
+AND (Class.CLASS_NO=Student.CLASS_NO)
+AND (Student.S_NO=Choice.S_NO)
+AND CLASS_DEPT='¼ÆËã»úÏµ'
