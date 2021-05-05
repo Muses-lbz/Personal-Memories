977d59519bb63f62399241d81723c0cf0a12aa2d
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-05-05 19:55:28 +0800

    Add files via upload

---
+++ 数据库原理与数据结构.sql
@@ -0,0 +1,11 @@
+﻿SELECT DISTINCT Teacher.T_NAME
+FROM Teacher,Teaching,Course
+WHERE Teaching.T_NO=Teacher.T_NO
+AND Course.COURSE_NO=Teaching.COURSE_NO
+AND COURSE_NAME ='数据库原理与应用' 
+UNION
+SELECT DISTINCT Teacher.T_NAME
+FROM Teacher,Teaching,Course
+WHERE Teaching.T_NO=Teacher.T_NO
+AND Course.COURSE_NO=Teaching.COURSE_NO
+AND COURSE_NAME ='数据结构' 
