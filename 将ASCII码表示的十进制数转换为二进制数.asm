01be26ec1f2b7f681a8adbab0991d7f6b9bbcaf9
Muses-lbz <74306793+Muses-lbz@users.noreply.github.com>
2021-10-19 17:42:31 +0800

    Add files via upload

---
+++ 将ASCII码表示的十进制数转换为二进制数.asm
@@ -0,0 +1,37 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+;将ASCII码表示的十进制数转换为二进制数
+SSTACK SEGMENT STACK
+    DW 64 DUP(?)
+SSTACK ENDS
+
+DATA   SEGMENT
+SADD   DB 30H,30H,32H,35H,36H    
+DATA   ENDS
+
+CODE   SEGMENT
+       ASSUME CS:CODE,DS:DATA
+START: MOV AX,DATA
+       MOV DS,AX
+       MOV AX,OFFSET SADD
+       MOV SI,AX
+       MOV BX,000AH
+       MOV CX,0004H
+       MOV AH,00H
+       MOV AL,[SI]
+       SUB AL,30H
+      
+A1:    IMUL BX
+       MOV DX,[SI+01]
+       AND DX,00FFH
+       ADC AX,DX
+       SUB AL,30H
+       INC SI
+       LOOP A1
+   
+A2:    JMP A2
+
+CODE   ENDS
+       END START
